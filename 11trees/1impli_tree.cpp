3618c3c49824c69a981f803f920a2a655844888d
VISHAL SUBHASH BIRADAR <143383723+vishalsb1@users.noreply.github.com>
2024-10-07 17:36:10 +0000

    edited the prepost , inorder tree making

--- 11trees/1impli_tree.cpp
+++ 11trees/1impli_tree.cpp
@@ -128,9 +128,64 @@ void Level_ordered_traversal(node *n1){
         }
     }
 }
+
+// implementing an tree from preorder and in order traversal also implementing an tree from preorder and in order traversal
+int find_index(int inorder[],int size,int target){
+    for(int i=0;i<size;i++){
+        if(inorder[i]==target){
+            return i;
+        }
+    }
+    return -1;
+}
+
+
+node* create_tree_from_inorder_preorder(int preorder[],int inorder[],int inorderstart,int inorderend, int &preorderIndex,int size){
+    if( preorderIndex>=size || inorderstart>inorderend ){
+        return NULL;
+    }
+
+    // ek condition 
+
+    int element=preorder[preorderIndex];
+    preorderIndex++;
+
+    node* root_node=new node(element);
+
+    int position=find_index(inorder,size,element);
+
+
+    root_node->left=create_tree_from_inorder_preorder(preorder,inorder,inorderstart,position-1,preorderIndex,size);
+    root_node->right=create_tree_from_inorder_preorder(preorder,inorder,position+1,inorderend,preorderIndex,size);
+
+    return root_node;
+}
+
+
+
+node* create_tree_from_inorder_postorder(int postorder[],int inorder[],int inorderstart,int inorderend, int &postorderIndex,int size){
+    if( postorderIndex>=size || inorderstart>inorderend ){
+        return NULL;
+    }
+
+    // ek condition 
+
+    int element=postorder[postorderIndex];
+    postorderIndex--;
+
+    node* root_node=new node(element);
+
+    int position=find_index(inorder,size,element);
+
+
+    root_node->right=create_tree_from_inorder_postorder(postorder,inorder,position+1,inorderend,postorderIndex,size);
+    root_node->left=create_tree_from_inorder_postorder(postorder,inorder,inorderstart,position-1,postorderIndex,size);
+
+    return root_node;
+}
 int main(){
 
-    node *n1=create_tree();
+    // node *n1=create_tree();
     // cout<<n1->data;
 
     // print_inorder_traversal(n1);
@@ -140,8 +195,34 @@ int main(){
 
     // 2. bfs breath first search(level wise)
     // BFS(n1);
-    Level_ordered_traversal(n1);  // hai wala level wise print karay sathi fkt manje level 0 nanter a endl
+    // Level_ordered_traversal(n1);  // hai wala level wise print karay sathi fkt manje level 0 nanter a endl
     // level 1 nanter parat ek endl 
     // and so on 
+// ---------------------------------------------------------------------------------------------------------------------------------------------------------------
+    // implementing an tree from preorder and in order traversal
+
+    // int preorder[]={2,8,10,6,4,12};
+    // int inorder[]={10,8,6,2,4,12};
+    // int inorderstart=0;
+    // int inorderend=5;
+    // int preorderIndex=0;
+    // int size=6;
+    // node* node1=create_tree_from_inorder_preorder(preorder,inorder,inorderstart,inorderend,preorderIndex,size);
+// ---------------------------------------------------------------------------------------------------------------------------------------------------------------
+    // implementing an tree from postorder and in order traversal 
+
+    int postorder[]={10,6,8,12,4,2};
+    int inorder[]={10,8,6,2,4,12};
+    int inorderstart=0;
+    int inorderend=5;
+    int size=6;
+    int postorderIndex=size-1;
+
+    node* node1=create_tree_from_inorder_postorder(postorder,inorder,inorderstart,inorderend,postorderIndex,size);
+
+
+    Level_ordered_traversal(node1);
+
+
     return 0;
 }
